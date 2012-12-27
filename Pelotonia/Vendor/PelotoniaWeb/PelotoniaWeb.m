139ac454baca5b5ee030af9176737723881dddd3
Mark Harris <mark@sandlot>
2012-12-27 12:18:38 -0500

    update parser to not freak out when we hit a problem parsing HTML from web site. (screen scraping)

--- Pelotonia/Vendor/PelotoniaWeb/PelotoniaWeb.m
+++ Pelotonia/Vendor/PelotoniaWeb/PelotoniaWeb.m
@@ -98,40 +98,42 @@
     
     [request setCompletionBlock:^{
         NSLog(@"Found rider %@", rider.name);
+        @try
+        {
         
-        TFHpple *parser = [TFHpple hppleWithHTMLData:[request responseData]];
+            TFHpple *parser = [TFHpple hppleWithHTMLData:[request responseData]];
+            
+            // figure out what type of rider we are first
+            NSString *riderTypeUrlXPath = @"//*[@id='sectionheader']/img";
+            NSString *riderType = [[[[parser searchWithXPathQuery:riderTypeUrlXPath] objectAtIndex:0] attributes] valueForKey:@"alt"];
+            
+            NSRange superPelotonRange = [riderType rangeOfString:@"Profile > Super Peloton" options:NSCaseInsensitiveSearch];
+            NSRange pelotonRange = [riderType rangeOfString:@"Profile > Peloton" options:NSCaseInsensitiveSearch];
+            NSRange riderRange = [riderType rangeOfString:@"Profile > Rider" options:NSCaseInsensitiveSearch];
+            NSRange virtualRiderRange = [riderType rangeOfString:@"Profile > Virtual" options:NSCaseInsensitiveSearch];
+            NSRange volunteerRiderRange = [riderType rangeOfString:@"Profile > Volunteer" options:NSCaseInsensitiveSearch];
+            
+            if (superPelotonRange.location != NSNotFound) {
+                rider.riderType = @"Super Peloton";
+            } else if (pelotonRange.location != NSNotFound) {
+                rider.riderType = @"Peloton";
+            } else if (riderRange.location != NSNotFound) {
+                rider.riderType = @"Rider";
+            } else if (virtualRiderRange.location != NSNotFound) {
+                rider.riderType = @"Virtual Rider";
+            } else if (volunteerRiderRange.location != NSNotFound) {
+                rider.riderType = @"Volunteer";
+            } else {
+                NSLog(@"rider type not recognized");
+                rider.riderType = nil;
+            }
         
-        // figure out what type of rider we are first
-        NSString *riderTypeUrlXPath = @"//*[@id='sectionheader']/img";
-        NSString *riderType = [[[[parser searchWithXPathQuery:riderTypeUrlXPath] objectAtIndex:0] attributes] valueForKey:@"alt"];
-        
-        NSRange superPelotonRange = [riderType rangeOfString:@"Profile > Super Peloton" options:NSCaseInsensitiveSearch];
-        NSRange pelotonRange = [riderType rangeOfString:@"Profile > Peloton" options:NSCaseInsensitiveSearch];
-        NSRange riderRange = [riderType rangeOfString:@"Profile > Rider" options:NSCaseInsensitiveSearch];
-        NSRange virtualRiderRange = [riderType rangeOfString:@"Profile > Virtual" options:NSCaseInsensitiveSearch];
-        NSRange volunteerRiderRange = [riderType rangeOfString:@"Profile > Volunteer" options:NSCaseInsensitiveSearch];
-        
-        if (superPelotonRange.location != NSNotFound) {
-            rider.riderType = @"Super Peloton";
-        } else if (pelotonRange.location != NSNotFound) {
-            rider.riderType = @"Peloton";
-        } else if (riderRange.location != NSNotFound) {
-            rider.riderType = @"Rider";
-        } else if (virtualRiderRange.location != NSNotFound) {
-            rider.riderType = @"Virtual Rider";
-        } else if (volunteerRiderRange.location != NSNotFound) {
-            rider.riderType = @"Volunteer";
-        }
-        
-        
-        // get the photos
-        NSString *riderPhotoUrlXPath = @"//div[@id='touts']/div[1]/img";
-        NSString *riderPhotoRelativeUrl = [[[[parser searchWithXPathQuery:riderPhotoUrlXPath] objectAtIndex:0] attributes] valueForKey:@"src"];
-        NSString *riderPhotoAbsoluteUrl = [NSString stringWithFormat:@"https://www.mypelotonia.org/%@", riderPhotoRelativeUrl];
-        rider.riderPhotoUrl = riderPhotoAbsoluteUrl;
-        
-        // get the rider's story
-        @try {
+            // get the photos
+            NSString *riderPhotoUrlXPath = @"//div[@id='touts']/div[1]/img";
+            NSString *riderPhotoRelativeUrl = [[[[parser searchWithXPathQuery:riderPhotoUrlXPath] objectAtIndex:0] attributes] valueForKey:@"src"];
+            NSString *riderPhotoAbsoluteUrl = [NSString stringWithFormat:@"https://www.mypelotonia.org/%@", riderPhotoRelativeUrl];
+            rider.riderPhotoUrl = riderPhotoAbsoluteUrl;
+            
             NSString *riderStoryXPath = @"//div[@class='story']/text()";
             NSArray *storyRows = [parser searchWithXPathQuery:riderStoryXPath];
             NSLog(@"story size %d", [storyRows count]);
@@ -139,51 +141,53 @@
             for (TFHppleElement *element in storyRows) {
                 rider.story = [rider.story stringByAppendingString:[element content]];
             }
+
+            // get the rider's properties off the page now
+            NSString *metaDataXPath = @"//div[@id='article']/div/div/div/dl";
+            NSArray *metaDataFields = [parser searchWithXPathQuery:metaDataXPath];
+            
+            for (TFHppleElement *metaDataElement in metaDataFields) {
+                NSString *header = [[[metaDataElement firstChild] firstChild] content];
+                
+                if (header) {
+                    TFHppleElement *content = [[[metaDataElement children] objectAtIndex:2] firstChild];
+                    
+                    if ([header isEqualToString:@"I've Raised:"]) { // volunteer/rider/virtual rider
+                        NSString *amountRaised = [content content];
+                        rider.amountRaised = amountRaised;
+                    } else if ([header isEqualToString:@"My Peloton:"]) { // rider/virtual rider
+                        NSString *myPeloton = [[content firstChild] content];
+                        rider.myPeloton = myPeloton;
+                    } else if ([header isEqualToString:@"Route I'm Riding:"]) { // rider
+                        NSString *route = [self stripWhitespace:[content content]];
+                        rider.route = route;
+                    } else if ([header isEqualToString:@"Peloton Funds Raised:"]) { // peloton
+                        NSString *pelotonFundsRaised = [content content];
+                        rider.pelotonFundsRaised = pelotonFundsRaised;
+                    } else if ([header isEqualToString:@"Total of All Members:"]) { // peloton
+                        NSString *pelotonTotalOfAllMembers = [content content];
+                        rider.pelotonTotalOfAllMembers = pelotonTotalOfAllMembers;
+                    } else if ([header isEqualToString:@"Grand Total Raised:"]) { // peloton
+                        NSString *pelotonGrandTotal = [content content];
+                        rider.pelotonGrandTotal = pelotonGrandTotal;
+                    } else if ([header isEqualToString:@"Peloton Captain:"]) { // peloton
+                        NSString *pelotonCaptain = [self stripWhitespace:[[[[[[metaDataElement children] objectAtIndex:2] children] objectAtIndex:1] firstChild] content]];
+                        rider.pelotonCaptain = pelotonCaptain;
+                    }
+                }
+            }
+            NSLog(@"Returning profile for rider %@", rider.name);
+
         }
         @catch (NSException *exception) {
             NSLog(@"trouble parsing %@'s profile story", rider.name);
             rider.story = @"(no story on file)";
-        }        
-
-        // get the rider's properties off the page now
-        NSString *metaDataXPath = @"//div[@id='article']/div/div/div/dl";
-        NSArray *metaDataFields = [parser searchWithXPathQuery:metaDataXPath];
-        
-        for (TFHppleElement *metaDataElement in metaDataFields) {
-            NSString *header = [[[metaDataElement firstChild] firstChild] content];
-            
-            if (header) {
-                TFHppleElement *content = [[[metaDataElement children] objectAtIndex:2] firstChild];
-                
-                if ([header isEqualToString:@"I've Raised:"]) { // volunteer/rider/virtual rider
-                    NSString *amountRaised = [content content];
-                    rider.amountRaised = amountRaised;
-                } else if ([header isEqualToString:@"My Peloton:"]) { // rider/virtual rider
-                    NSString *myPeloton = [[content firstChild] content];
-                    rider.myPeloton = myPeloton;
-                } else if ([header isEqualToString:@"Route I'm Riding:"]) { // rider
-                    NSString *route = [self stripWhitespace:[content content]];
-                    rider.route = route;
-                } else if ([header isEqualToString:@"Peloton Funds Raised:"]) { // peloton
-                    NSString *pelotonFundsRaised = [content content];
-                    rider.pelotonFundsRaised = pelotonFundsRaised;
-                } else if ([header isEqualToString:@"Total of All Members:"]) { // peloton
-                    NSString *pelotonTotalOfAllMembers = [content content];
-                    rider.pelotonTotalOfAllMembers = pelotonTotalOfAllMembers;
-                } else if ([header isEqualToString:@"Grand Total Raised:"]) { // peloton
-                    NSString *pelotonGrandTotal = [content content];
-                    rider.pelotonGrandTotal = pelotonGrandTotal;
-                } else if ([header isEqualToString:@"Peloton Captain:"]) { // peloton
-                    NSString *pelotonCaptain = [self stripWhitespace:[[[[[[metaDataElement children] objectAtIndex:2] children] objectAtIndex:1] firstChild] content]];
-                    rider.pelotonCaptain = pelotonCaptain;
-                }
-            }
+            rider.riderType = @"Unknown";
         }
-
+     
         if (completeBlock) {
             completeBlock(rider);
         }
-        NSLog(@"Returning profile for rider %@", rider.name);
     }];
     
     [request setFailedBlock:^{
