f99d95b62702e10b07017d75b8d794406cb2aae6
Tina <tsiswandari@manta.com>
2014-01-30 19:56:23 -0500

    WIP: Navigation Drawer Fragments Place holder

--- android/Pelotonia/src/main/java/org/pelotonia/pelotonia/dummy/DummyContent.java
+++ android/Pelotonia/src/main/java/org/pelotonia/pelotonia/dummy/DummyContent.java
@@ -25,8 +25,8 @@ public class DummyContent {
 
     static {
         // Add 3 sample items.
-        addItem(new DummyItem("1", "Item 1"));
-        addItem(new DummyItem("2", "Item 2"));
+        addItem(new DummyItem("1", "Item X"));
+        addItem(new DummyItem("2", "Item Y"));
         addItem(new DummyItem("3", "Item 3"));
     }
 
